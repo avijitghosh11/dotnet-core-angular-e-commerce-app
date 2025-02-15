04ec3d6341518d2b6e76bc5dc660324ae0e9dead
Avijit Ghosh <avijitghsh11@gmail.com>
2025-02-15 14:14:55 +0530

    Section 2 done

---
+++ src/Ekart.Server/Ekart.Api/Program.cs
@@ -0,0 +1,32 @@
+using Ekart.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+builder.Services.AddDbContext<StoreContext>(options=>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+app.UseSwaggerUI(opt=>opt.SwaggerEndpoint("/openapi/v1.json","Ekart.Api"));
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ src/Ekart.Server/Ekart.Core/Entites/BaseEntity.cs
@@ -0,0 +1,7 @@
+﻿namespace Ekart.Core.Entites
+{
+    public class BaseEntity
+    {
+        public int Id { get; set; }
+    }
+}
